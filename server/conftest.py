521d6509a1139e15a08a2db45021654f055cedcc
Paul Schleifer <paul_schleifer@hotmail.com>
2025-02-10 23:05:12 +0000

    streamline server tests

---
+++ server/conftest.py
@@ -0,0 +1,15 @@
+""" This file is used to define fixtures that can be used in the tests. """
+import pytest
+from src.app import app as flask_app
+
+
+@pytest.fixture
+def app():
+    """ Create a Flask app for testing. """
+    yield flask_app
+
+
+@pytest.fixture
+def client(app):
+    """ Create a test client for the Flask app. """
+    return app.test_client()
